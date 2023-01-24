add84780807cbbe061cfdb27d485d39ad8a059dd
abhijeet1069 <abhijeet1069@gmail.com>
2023-01-24 21:02:06 +0530

    new commit

---
+++ NatureOfCode/NOC/1-vectors/1.3bouncingball-vector-operations/vector-sub.js
@@ -0,0 +1,16 @@
+function setup(){
+    createCanvas(640, 360);
+}
+
+function draw(){
+    background(51);
+    let mouse = createVector(mouseX, mouseY);
+    let center = createVector(width/2, height/2);
+
+    mouse.sub(center);
+
+    translate(width/2, height/2);
+    strokeWeight(2);
+    stroke(255);
+    line(0,0,mouse.x,mouse.y);
+}
